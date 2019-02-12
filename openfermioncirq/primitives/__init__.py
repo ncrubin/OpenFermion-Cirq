e71c96929c679d4aefd6b1c70cd7e689a063d1d2
Nick Rubin <rubinnc0@gmail.com>
2019-02-11 21:11:09 -0800

    Implementation of the optimal givens rotation network.

--- openfermioncirq/primitives/__init__.py
+++ openfermioncirq/primitives/__init__.py
@@ -14,6 +14,8 @@
 
 from openfermioncirq.primitives.bogoliubov_transform import bogoliubov_transform
 
+from openfermioncirq.primitives.optimal_givens_decomposition import front_and_back_givens_decomposition
+
 from openfermioncirq.primitives.state_preparation import (
     prepare_gaussian_state,
     prepare_slater_determinant)
